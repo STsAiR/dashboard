e80689a3f9249879f40ba1e938b0ce65d0ff8eb2
Nam KOO <stsair@NamdeMacBook-Air.local>
2024-10-23 17:54:00 +0800

    modify template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import Message from "./Message";
+import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
 
 function App() {
+    // let items = ["apple", "banana", "cherry", "date", "elderberry"];
+    // const handleSelectItem = (item: string) => console.log(item);
+
+    // text is a html content
+    // const text = "<h1>This is an alert!</h1>";
+
+    // const text = "This is an alert!";
     return (
         <div>
-            <Message />
+            {/* <ListGroup
+                items={items}
+                heading="Fruits"
+                onSelectItem={handleSelectItem}
+            /> */}
+            <Alert>
+                {/* Hello */}
+                <h1>World</h1>
+            </Alert>
         </div>
     );
 }
